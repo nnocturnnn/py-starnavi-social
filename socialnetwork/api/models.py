98072fe975b48e791cfde83e3dcaf7f43343208e
nnnocturnn <mediandrey@gmail.com>
2024-05-25 18:05:46 +0100

    feat: create models

--- socialnetwork/api/models.py
+++ socialnetwork/api/models.py
@@ -1,3 +1,24 @@
-from django.db import models  # noqa: F401
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    last_activity = models.DateTimeField(null=True, blank=True)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
